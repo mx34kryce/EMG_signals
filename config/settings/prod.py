657f72e42beea13e06b9e5cf69ff981219ac0c58
mx34kryce <jong1334@khu.ac.kr>
2023-08-10 21:06:37 +0900

    Hosts changed by Server reboot

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,3 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.35.210.215','ec2-3-35-210-215.ap-northeast-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['54.180.159.173','ec2-54-180-159-173.ap-northeast-2.compute.amazonaws.com']
